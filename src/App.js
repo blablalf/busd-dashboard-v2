0175d5895126bc98afbbbee966ea5074cc2a6ede
blablalf <a.gaillard@ra2.tech>
2024-09-03 20:08:36 +0200

    style: fix unused import

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import {
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query"
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import './App.css';
+import "./App.css";
 
-import React from 'react';
+import React from "react";
 
-import Header from './components/Header.jsx';
+import Header from "./components/Header.jsx";
 import Modals from "./components/Modals.jsx";
 import Home from "./components/Home.jsx";
 import { Toaster } from "react-hot-toast";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 function App() {
   return (

b13407a529104358a8fa31aa1fcfeb33abee7161
blablalf <a.gaillard@ra2.tech>
2024-09-05 13:36:41 +0200

    fix: mint toast

--- src/hooks/useMintToken.js
+++ src/hooks/useMintToken.js
@@ -3,6 +3,7 @@ import { useMutation } from "@tanstack/react-query";
 import { mintToken, getTokenAddress } from "../adapters/ClientsAdapter.js";
 import useGetUserAddress from "./useGetUserAddress.js";
 import toast from "react-hot-toast";
+import TransactionToast from "../components/TransactionToast/TransactionToast.jsx";
 
 export default function useMintToken() {
   const tokenAddress = getTokenAddress();
@@ -10,7 +11,7 @@ export default function useMintToken() {
   return useMutation({
     mutationFn: (amount) => mintToken(tokenAddress, userAddress, amount),
     onSuccess: (hash) => {
-      toast.success("Mint transaction sent. Transaction hash: " + hash);
+      toast.custom(<TransactionToast message="Mint transaction sent. Transaction hash:" hash={hash} />);
     },
     onError: (error) => {
       toast.error("Mint transaction failed");
